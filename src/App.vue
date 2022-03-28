12beb9bfabbe20da95adc7283825d79bb3bdf43a
amordz72 <dzamor72@gmail.com>
2022-03-28 14:27:33 +0100

    1 c

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+<div>
+    <Navbar></Navbar>
+</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ 
+
+}
+</style>
