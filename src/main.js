18e70c8368e44c14c583fdc11541ed607199c7d9
amordz72 <dzamor72@gmail.com>
2022-03-28 21:24:54 +0100

    d

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'  
 import store from "./store/store"
-import store_2 from "./store/store_2"
 import axios from 'axios'
 
 
-createApp(App).use(router).use(store).use(store_2).use(axios).mount('#app')
+createApp(App).use(router).use(store).use(axios).mount('#app')
