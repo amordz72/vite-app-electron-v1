dcc4d099f933f66b4c1b725c6feb4a008a4c3ab4
amordz72 <dzamor72@gmail.com>
2022-03-28 21:08:20 +0100

    add-stor

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1> Abaut Page</h1>
+  <h1>Abaut Page</h1>
+  <h2>{{ data }}</h2>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      data: this.$store.state.name,
+    };
+  },
+};
+</script>
